37d5c78119a2f85238ce12dd66804b24533e6e7f
basil lubbad <87727129+basillubbad@users.noreply.github.com>
2022-11-28 17:51:33 +0200

    first

---
+++ sarvic/servic.js
@@ -0,0 +1,19 @@
+function mouseOver1() {
+    document.getElementById("arow1").style.marginRight="29px";
+  }
+  function mouseOut1() {
+    document.getElementById("arow1").style.marginRight="11px";
+  }
+  
+  function mouseOver2() {
+    document.getElementById("arow2").style.marginRight="29px";
+  }
+  function mouseOut2() {
+    document.getElementById("arow2").style.marginRight="11px";
+  }
+  function mouseOver3() {
+    document.getElementById("arow3").style.marginRight="29px";
+  }
+  function mouseOut3() {
+    document.getElementById("arow3").style.marginRight="11px";
+  }
